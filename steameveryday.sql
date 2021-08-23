767453a810f75923868f3f57512a3080b227d309
gilarndw <60825743+gilarndw@users.noreply.github.com>
2021-08-23 10:34:15 +0700

    Add files via upload

--- steameveryday.sql
+++ steameveryday.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v11.11 (64 bit)
-MySQL - 5.5.32 : Database - steameveryday
+MySQL - 5.5.32 : Database - steameveryday2
 *********************************************************************
 */
 
@@ -12,6 +12,10 @@ MySQL - 5.5.32 : Database - steameveryday
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`steameveryday2` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `steameveryday2`;
+
 /*Table structure for table `barang` */
 
 CREATE TABLE `barang` (
@@ -23,7 +27,7 @@ CREATE TABLE `barang` (
 
 /*Data for the table `barang` */
 
-insert  into `barang`(`ID_BARANG`,`NAMA_BARANG`,`STOCK`) values (1,'Shampoo',79),(2,'Pewangi',91),(3,'Semir Ban',98),(4,'Pengkilat Kit',89);
+insert  into `barang`(`ID_BARANG`,`NAMA_BARANG`,`STOCK`) values (1,'Shampoo',79),(2,'Pewangi',91),(3,'Semir Ban',97),(4,'Pengkilat Kit',89);
 
 /*Table structure for table `jasa` */
 
@@ -70,7 +74,7 @@ CREATE TABLE `jasadikerjakan` (
 
 /*Data for the table `jasadikerjakan` */
 
-insert  into `jasadikerjakan`(`ID_PENCUCI`,`ID_DETAIL_TRANSAKSI`) values (1,1),(3,1),(2,2),(1,3),(2,3),(2,4),(3,4),(1,5),(3,5),(1,6),(2,6),(2,7),(3,7),(1,8),(3,8),(1,9),(2,9),(3,10),(1,11),(2,12),(3,12);
+insert  into `jasadikerjakan`(`ID_PENCUCI`,`ID_DETAIL_TRANSAKSI`) values (1,1),(3,1),(2,2),(1,3),(2,3),(2,4),(3,4),(1,5),(3,5),(1,6),(2,6),(2,7),(3,7),(1,8),(3,8),(1,9),(2,9),(3,10),(1,11),(2,12),(3,12),(2,13),(3,14);
 
 /*Table structure for table `kasir` */
 
@@ -122,7 +126,7 @@ CREATE TABLE `menggunakanbarang` (
 
 /*Data for the table `menggunakanbarang` */
 
-insert  into `menggunakanbarang`(`ID_BARANG`,`ID_DETAIL_TRANSAKSI`,`BANYAK_DIGUNAKAN`) values (1,1,2),(1,3,2),(1,4,2),(1,5,2),(1,6,2),(1,7,2),(1,8,2),(1,9,3),(1,10,1),(1,12,2),(1,13,1),(2,1,1),(2,3,1),(2,4,1),(2,5,1),(2,6,1),(2,7,1),(2,8,1),(2,9,1),(2,12,1),(3,2,1),(3,11,1),(4,1,1),(4,3,1),(4,4,1),(4,5,1),(4,6,1),(4,7,1),(4,8,1),(4,9,1),(4,10,1),(4,12,1),(4,13,1);
+insert  into `menggunakanbarang`(`ID_BARANG`,`ID_DETAIL_TRANSAKSI`,`BANYAK_DIGUNAKAN`) values (1,1,2),(1,3,2),(1,4,2),(1,5,2),(1,6,2),(1,7,2),(1,8,2),(1,9,3),(1,10,1),(1,12,2),(1,13,1),(2,1,1),(2,3,1),(2,4,1),(2,5,1),(2,6,1),(2,7,1),(2,8,1),(2,9,1),(2,12,1),(3,2,1),(3,11,1),(3,14,1),(4,1,1),(4,3,1),(4,4,1),(4,5,1),(4,6,1),(4,7,1),(4,8,1),(4,9,1),(4,10,1),(4,12,1),(4,13,1);
 
 /*Table structure for table `merekkenderaan` */
 
@@ -164,11 +168,11 @@ CREATE TABLE `pemasukkan` (
   PRIMARY KEY (`ID_PEMASUKKAN`),
   KEY `ID_DETAIL_TRANSAKSI` (`ID_DETAIL_TRANSAKSI`),
   CONSTRAINT `pemasukkan_ibfk_1` FOREIGN KEY (`ID_DETAIL_TRANSAKSI`) REFERENCES `transaksijasa` (`ID_DETAIL_TRANSAKSI`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 /*Data for the table `pemasukkan` */
 
-insert  into `pemasukkan`(`ID_PEMASUKKAN`,`ID_DETAIL_TRANSAKSI`,`DESKRIPSI`,`NILAI`,`TANGGAL`) values (1,1,'Pemasukkan dari transaksi 1#1',55000,'2018-01-09 10:12:37'),(2,2,'Pemasukkan dari transaksi 1#2',15000,'2018-01-09 10:14:00'),(3,3,'Pemasukkan dari transaksi 2#3',55000,'2018-01-09 10:26:34'),(4,4,'Pemasukkan dari transaksi 3#4',55000,'2018-01-09 10:26:46'),(5,5,'Pemasukkan dari transaksi 4#5',55000,'2018-01-09 10:26:54'),(6,6,'Pemasukkan dari transaksi 5#6',55000,'2018-01-09 10:27:02'),(7,7,'Pemasukkan dari transaksi 6#7',55000,'2018-01-09 10:27:08'),(8,8,'Pemasukkan dari transaksi 7#8',0,'2018-01-09 10:27:15'),(9,9,'Pemasukkan dari transaksi 8#9',100000,'2018-01-22 12:08:27'),(10,10,'Pemasukkan dari transaksi 9#10',30000,'2018-01-22 12:14:38'),(11,11,'Pemasukkan dari transaksi 10#11',15000,'2018-01-22 12:15:36'),(12,12,'Pemasukkan dari transaksi 11#12',50000,'2018-01-22 22:34:11'),(13,13,'Pemasukkan dari transaksi 12#13',20000,'2018-01-22 22:36:07');
+insert  into `pemasukkan`(`ID_PEMASUKKAN`,`ID_DETAIL_TRANSAKSI`,`DESKRIPSI`,`NILAI`,`TANGGAL`) values (1,1,'Pemasukkan dari transaksi 1#1',55000,'2018-01-09 10:12:37'),(2,2,'Pemasukkan dari transaksi 1#2',15000,'2018-01-09 10:14:00'),(3,3,'Pemasukkan dari transaksi 2#3',55000,'2018-01-09 10:26:34'),(4,4,'Pemasukkan dari transaksi 3#4',55000,'2018-01-09 10:26:46'),(5,5,'Pemasukkan dari transaksi 4#5',55000,'2018-01-09 10:26:54'),(6,6,'Pemasukkan dari transaksi 5#6',55000,'2018-01-09 10:27:02'),(7,7,'Pemasukkan dari transaksi 6#7',55000,'2018-01-09 10:27:08'),(8,8,'Pemasukkan dari transaksi 7#8',0,'2018-01-09 10:27:15'),(9,9,'Pemasukkan dari transaksi 8#9',100000,'2018-01-22 12:08:27'),(10,10,'Pemasukkan dari transaksi 9#10',30000,'2018-01-22 12:14:38'),(11,11,'Pemasukkan dari transaksi 10#11',15000,'2018-01-22 12:15:36'),(12,12,'Pemasukkan dari transaksi 11#12',50000,'2018-01-22 22:34:11'),(13,13,'Pemasukkan dari transaksi 12#13',20000,'2018-01-22 22:36:07'),(14,14,'Pemasukkan dari transaksi 12#14',15000,'2018-01-23 01:59:26');
 
 /*Table structure for table `pencuci` */
 
@@ -195,11 +199,11 @@ CREATE TABLE `pengeluaran` (
   PRIMARY KEY (`ID_PENGELUARAN`),
   KEY `FK_PENGELUARANSTEAM` (`ID_DETAIL_TRANSAKSI`),
   CONSTRAINT `FK_PENGELUARANSTEAM` FOREIGN KEY (`ID_DETAIL_TRANSAKSI`) REFERENCES `transaksijasa` (`ID_DETAIL_TRANSAKSI`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 /*Data for the table `pengeluaran` */
 
-insert  into `pengeluaran`(`ID_PENGELUARAN`,`ID_DETAIL_TRANSAKSI`,`DESKRIPSI`,`NILAI`,`TANGGAL`) values (1,1,'Pengeluaran untuk upah pencuci dari transaksi 1#1',15000,'2018-01-09 10:12:37'),(2,2,'Pengeluaran untuk upah pencuci dari transaksi 1#2',8000,'2018-01-09 10:14:00'),(3,3,'Pengeluaran untuk upah pencuci dari transaksi 2#3',15000,'2018-01-09 10:26:34'),(4,4,'Pengeluaran untuk upah pencuci dari transaksi 3#4',15000,'2018-01-09 10:26:46'),(5,5,'Pengeluaran untuk upah pencuci dari transaksi 4#5',15000,'2018-01-09 10:26:55'),(6,6,'Pengeluaran untuk upah pencuci dari transaksi 5#6',15000,'2018-01-09 10:27:02'),(7,7,'Pengeluaran untuk upah pencuci dari transaksi 6#7',15000,'2018-01-09 10:27:08'),(8,8,'Pengeluaran untuk upah pencuci dari transaksi 7#8',15000,'2018-01-09 10:27:16'),(9,9,'Pengeluaran untuk upah pencuci dari transaksi 8#9',40000,'2018-01-22 12:08:27'),(10,10,'Pengeluaran untuk upah pencuci dari transaksi 9#10',10000,'2018-01-22 12:14:38'),(11,11,'Pengeluaran untuk upah pencuci dari transaksi 10#11',8000,'2018-01-22 12:15:36'),(12,12,'Pengeluaran untuk upah pencuci dari transaksi 11#12',15000,'2018-01-22 22:34:11'),(13,13,'Pengeluaran untuk upah pencuci dari transaksi 12#13',5000,'2018-01-22 22:36:07');
+insert  into `pengeluaran`(`ID_PENGELUARAN`,`ID_DETAIL_TRANSAKSI`,`DESKRIPSI`,`NILAI`,`TANGGAL`) values (1,1,'Pengeluaran untuk upah pencuci dari transaksi 1#1',15000,'2018-01-09 10:12:37'),(2,2,'Pengeluaran untuk upah pencuci dari transaksi 1#2',8000,'2018-01-09 10:14:00'),(3,3,'Pengeluaran untuk upah pencuci dari transaksi 2#3',15000,'2018-01-09 10:26:34'),(4,4,'Pengeluaran untuk upah pencuci dari transaksi 3#4',15000,'2018-01-09 10:26:46'),(5,5,'Pengeluaran untuk upah pencuci dari transaksi 4#5',15000,'2018-01-09 10:26:55'),(6,6,'Pengeluaran untuk upah pencuci dari transaksi 5#6',15000,'2018-01-09 10:27:02'),(7,7,'Pengeluaran untuk upah pencuci dari transaksi 6#7',15000,'2018-01-09 10:27:08'),(8,8,'Pengeluaran untuk upah pencuci dari transaksi 7#8',15000,'2018-01-09 10:27:16'),(9,9,'Pengeluaran untuk upah pencuci dari transaksi 8#9',40000,'2018-01-22 12:08:27'),(10,10,'Pengeluaran untuk upah pencuci dari transaksi 9#10',10000,'2018-01-22 12:14:38'),(11,11,'Pengeluaran untuk upah pencuci dari transaksi 10#11',8000,'2018-01-22 12:15:36'),(12,12,'Pengeluaran untuk upah pencuci dari transaksi 11#12',15000,'2018-01-22 22:34:11'),(13,13,'Pengeluaran untuk upah pencuci dari transaksi 12#13',5000,'2018-01-22 22:36:07'),(14,14,'Pengeluaran untuk upah pencuci dari transaksi 12#14',8000,'2018-01-23 01:59:26');
 
 /*Table structure for table `promo` */
 
@@ -243,7 +247,7 @@ CREATE TABLE `transaksi` (
   `ID_KASIR` int(11) DEFAULT NULL,
   `ID_TIPE` int(11) DEFAULT NULL,
   `ID_PELANGGAN` int(11) DEFAULT NULL,
-  `TANGGAL` date DEFAULT NULL,
+  `TANGGAL` datetime DEFAULT NULL,
   `NO_POLISI` varchar(12) DEFAULT NULL,
   PRIMARY KEY (`ID_TRANSAKSI`),
   KEY `FK_MEMILIKITIPE` (`ID_TIPE`),
@@ -256,7 +260,7 @@ CREATE TABLE `transaksi` (
 
 /*Data for the table `transaksi` */
 
-insert  into `transaksi`(`ID_TRANSAKSI`,`ID_KASIR`,`ID_TIPE`,`ID_PELANGGAN`,`TANGGAL`,`NO_POLISI`) values (1,1,1,1,'2018-01-09','B 1242 KLO'),(2,2,1,1,'2018-01-24','B 1242 KLO'),(3,1,1,1,'2018-02-27','B 1242 KLO'),(4,1,1,1,'2018-03-23','B 1242 KLO'),(5,2,1,1,'2018-04-09','B 1242 KLO'),(6,1,1,1,'2018-07-19','B 1242 KLO'),(7,1,1,1,'2018-11-01','B 1242 KLO'),(8,1,2,2,'2018-05-24','B 6151 MLS'),(9,2,8,3,'2018-07-12','B 2563 NNJ'),(10,2,7,4,'2018-01-03','B 1776 ITA'),(11,2,4,3,'2018-10-19','B 670 BTC'),(12,1,6,1,'2018-01-18','B 0089 STR');
+insert  into `transaksi`(`ID_TRANSAKSI`,`ID_KASIR`,`ID_TIPE`,`ID_PELANGGAN`,`TANGGAL`,`NO_POLISI`) values (1,1,1,1,'2018-01-09 00:00:00','B 1242 KLO'),(2,2,1,1,'2018-01-24 00:00:00','B 1242 KLO'),(3,1,1,1,'2018-02-27 00:00:00','B 1242 KLO'),(4,1,1,1,'2018-03-23 00:00:00','B 1242 KLO'),(5,2,1,1,'2018-04-09 00:00:00','B 1242 KLO'),(6,1,1,1,'2018-07-19 00:00:00','B 1242 KLO'),(7,1,1,1,'2018-11-01 00:00:00','B 1242 KLO'),(8,1,2,2,'2018-05-24 00:00:00','B 6151 MLS'),(9,2,8,3,'2018-07-12 00:00:00','B 2563 NNJ'),(10,2,7,4,'2018-01-03 00:00:00','B 1776 ITA'),(11,2,4,3,'2018-10-19 00:00:00','B 670 BTC'),(12,1,6,1,'2018-01-18 00:00:00','B 0089 STR');
 
 /*Table structure for table `transaksijasa` */
 
@@ -281,11 +285,11 @@ CREATE TABLE `transaksijasa` (
   CONSTRAINT `FK_MEMILIKIDETAILTRANSAKSI` FOREIGN KEY (`ID_TRANSAKSI`) REFERENCES `transaksi` (`ID_TRANSAKSI`) ON UPDATE CASCADE,
   CONSTRAINT `FK_PENGELUARANSTEAM2` FOREIGN KEY (`ID_PENGELUARAN`) REFERENCES `pengeluaran` (`ID_PENGELUARAN`) ON UPDATE CASCADE,
   CONSTRAINT `FK_TRANSAKSIMEMILIKIJASA` FOREIGN KEY (`ID_JASA`) REFERENCES `jasa` (`ID_JASA`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 /*Data for the table `transaksijasa` */
 
-insert  into `transaksijasa`(`ID_DETAIL_TRANSAKSI`,`ID_JASA`,`ID_TRANSAKSI`,`ID_PEMASUKKAN`,`ID_KUPON`,`ID_PENGELUARAN`,`PROMO_CHECK`,`HARGA`,`KEUNTUNGAN`,`UPAH`) values (1,4,1,1,NULL,1,1,55000,40000,15000),(2,6,1,2,NULL,2,0,15000,7000,8000),(3,4,2,3,NULL,3,1,55000,40000,15000),(4,4,3,4,NULL,4,1,55000,40000,15000),(5,4,4,5,NULL,5,1,55000,40000,15000),(6,4,5,6,NULL,6,1,55000,40000,15000),(7,4,6,7,NULL,7,1,55000,40000,15000),(8,4,7,8,1,8,1,0,0,15000),(9,5,8,9,NULL,9,0,100000,60000,40000),(10,1,9,10,NULL,10,0,30000,20000,10000),(11,6,10,11,NULL,11,0,15000,7000,8000),(12,3,11,12,NULL,12,0,50000,35000,15000),(13,2,12,13,NULL,13,0,20000,15000,5000);
+insert  into `transaksijasa`(`ID_DETAIL_TRANSAKSI`,`ID_JASA`,`ID_TRANSAKSI`,`ID_PEMASUKKAN`,`ID_KUPON`,`ID_PENGELUARAN`,`PROMO_CHECK`,`HARGA`,`KEUNTUNGAN`,`UPAH`) values (1,4,1,1,NULL,1,1,55000,40000,15000),(2,6,1,2,NULL,2,0,15000,7000,8000),(3,4,2,3,NULL,3,1,55000,40000,15000),(4,4,3,4,NULL,4,1,55000,40000,15000),(5,4,4,5,NULL,5,1,55000,40000,15000),(6,4,5,6,NULL,6,1,55000,40000,15000),(7,4,6,7,NULL,7,1,55000,40000,15000),(8,4,7,8,1,8,1,0,0,15000),(9,5,8,9,NULL,9,0,100000,60000,40000),(10,1,9,10,NULL,10,0,30000,20000,10000),(11,6,10,11,NULL,11,0,15000,7000,8000),(12,3,11,12,NULL,12,0,50000,35000,15000),(13,2,12,13,NULL,13,0,20000,15000,5000),(14,6,12,14,NULL,14,0,15000,7000,8000);
 
 /* Trigger structure for table `jasa` */
 
@@ -475,10 +479,55 @@ DROP TABLE IF EXISTS `viewpromocheck`;
  `PROMO_CHECK` tinyint(1) 
 )*/;
 
+/*Table structure for table `viewreportlaporandetail` */
+
+DROP TABLE IF EXISTS `viewreportlaporandetail`;
+
+/*!50001 CREATE TABLE  `viewreportlaporandetail`(
+ `ID_TRANSAKSI` int(11) ,
+ `ID_KASIR` int(11) ,
+ `NAMA_KASIR` varchar(255) ,
+ `ID_PELANGGAN` int(11) ,
+ `NAMA_PELANGGAN` varchar(255) ,
+ `ID_TIPE` int(11) ,
+ `TIPE KENDARAAN` varchar(255) ,
+ `NO_POLISI` varchar(12) ,
+ `TANGGAL` datetime ,
+ `ID_JASA` int(11) ,
+ `NAMA_JASA` varchar(255) ,
+ `ID_KUPON` int(11) ,
+ `HARGA` decimal(15,0) ,
+ `KEUNTUNGAN` decimal(15,0) ,
+ `UPAH` decimal(15,0) 
+)*/;
+
+/*Table structure for table `viewreportpemasukkan` */
+
+DROP TABLE IF EXISTS `viewreportpemasukkan`;
+
+/*!50001 CREATE TABLE  `viewreportpemasukkan`(
+ `JUMLAH_TRANSAKSI` bigint(21) ,
+ `JUMLAH_JASA_PER_TRANSAKSI` bigint(21) ,
+ `HARGA` decimal(37,0) ,
+ `KEUNTUNGAN` decimal(37,0) ,
+ `UPAH` decimal(37,0) ,
+ `TANGGAL` varchar(69) 
+)*/;
+
 /*View structure for view viewpromocheck */
 
 /*!50001 DROP TABLE IF EXISTS `viewpromocheck` */;
 /*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewpromocheck` AS (select `t`.`ID_TRANSAKSI` AS `ID_TRANSAKSI`,`tj`.`ID_JASA` AS `ID_JASA`,`t`.`ID_PELANGGAN` AS `ID_PELANGGAN`,`tj`.`PROMO_CHECK` AS `PROMO_CHECK` from ((`transaksijasa` `tj` join `transaksi` `t` on((`tj`.`ID_TRANSAKSI` = `t`.`ID_TRANSAKSI`))) join `pelanggan` `p` on((`t`.`ID_PELANGGAN` = `p`.`ID_PELANGGAN`))) where (`tj`.`PROMO_CHECK` = 0)) */;
+
+/*View structure for view viewreportlaporandetail */
+
+/*!50001 DROP TABLE IF EXISTS `viewreportlaporandetail` */;
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewreportlaporandetail` AS (select `t`.`ID_TRANSAKSI` AS `ID_TRANSAKSI`,`t`.`ID_KASIR` AS `ID_KASIR`,`k`.`NAMA` AS `NAMA_KASIR`,`t`.`ID_PELANGGAN` AS `ID_PELANGGAN`,`p`.`NAMA` AS `NAMA_PELANGGAN`,`t`.`ID_TIPE` AS `ID_TIPE`,`tp`.`NAMA` AS `TIPE KENDARAAN`,`t`.`NO_POLISI` AS `NO_POLISI`,`t`.`TANGGAL` AS `TANGGAL`,`tj`.`ID_JASA` AS `ID_JASA`,`j`.`NAMA` AS `NAMA_JASA`,`tj`.`ID_KUPON` AS `ID_KUPON`,`tj`.`HARGA` AS `HARGA`,`tj`.`KEUNTUNGAN` AS `KEUNTUNGAN`,`tj`.`UPAH` AS `UPAH` from ((((((`transaksi` `t` join `transaksijasa` `tj` on((`t`.`ID_TRANSAKSI` = `tj`.`ID_TRANSAKSI`))) join `pelanggan` `p` on((`t`.`ID_PELANGGAN` = `p`.`ID_PELANGGAN`))) join `tipekendaraan` `tp` on((`t`.`ID_TIPE` = `tp`.`ID_TIPE`))) join `kasir` `k` on((`t`.`ID_KASIR` = `k`.`ID_KASIR`))) join `jasa` `j` on((`tj`.`ID_JASA` = `j`.`ID_JASA`))) left join `kupon` `kp` on((`tj`.`ID_KUPON` = `kp`.`ID_KUPON`)))) */;
+
+/*View structure for view viewreportpemasukkan */
+
+/*!50001 DROP TABLE IF EXISTS `viewreportpemasukkan` */;
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `viewreportpemasukkan` AS (select count(distinct `t`.`ID_TRANSAKSI`) AS `JUMLAH_TRANSAKSI`,count(`tj`.`ID_DETAIL_TRANSAKSI`) AS `JUMLAH_JASA_PER_TRANSAKSI`,sum(`tj`.`HARGA`) AS `HARGA`,sum(`tj`.`KEUNTUNGAN`) AS `KEUNTUNGAN`,sum(`tj`.`UPAH`) AS `UPAH`,date_format(`t`.`TANGGAL`,'%Y-%M') AS `TANGGAL` from (`transaksijasa` `tj` join `transaksi` `t` on((`t`.`ID_TRANSAKSI` = `tj`.`ID_TRANSAKSI`))) group by year(`t`.`TANGGAL`),month(`t`.`TANGGAL`)) */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
